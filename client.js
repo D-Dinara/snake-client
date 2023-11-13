6feb43ec41c4cd3b683dc42a113208343405bf6c
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-13 12:36:33 -0500

    Add event handlers on connection to the server: to log a success message, to send the client's name to the server

--- client.js
+++ client.js
@@ -7,6 +7,16 @@ const connect = function() {
     port: 50541
   });
 
+  //event handler: console.log a message after the connection to the server is established
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
+  //event hadler: sends client's name to the server after the connection is established
+  conn.on("connect", () => {
+    conn.write("Name: DDI");
+  });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
