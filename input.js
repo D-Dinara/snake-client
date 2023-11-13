6c38187c2c7235a1d690670642e47bc3c902c947
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-13 14:24:08 -0500

    Add input.js to handle inputs

---
+++ input.js
@@ -0,0 +1,19 @@
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  //event listener for user inputs
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+// the function specifies what happens when a particular key is pressed on the keyboard input
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
